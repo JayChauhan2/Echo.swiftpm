594d8cf26fae591f10e9545fd747762cd4cc5eb6
JayChauhan2 <jayschauhan3@gmail.com>
2026-02-28 09:05:30 -0500

    + added logo plus some misc changes

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
             teamIdentifier: "3JB47XFYY5",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .flower),
-            accentColor: .presetColor(.pink),
+            appIcon: .asset("AppIcon"),
+            accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [
                 .pad,
                 .phone
